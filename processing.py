5e87164c888146c8a96e47d69ee2acc4469ea2c3
Barbara Wachek <barbara.wachek@gmail.com>
2024-02-25 12:39:52 +0100

    2024-02-25

--- processing.py
+++ processing.py
@@ -7,11 +7,31 @@ file_name = 'C:\\Users\\PBL_Basia\\Documents\\My scripts\\EV_chargers\\data\\sam
 dataframe = pd.read_excel(file_name)
 df_reversed = dataframe[::-1]
 
+df_reversed['Zewnętrzny numer ID karty '].unique()
+
+
+#DataFrame without few columns ('Data faktury', 'Event', 'Informacje uzupełniające', 'Moc (kW)',
+#        'Energia (kWh)', 'Czas trwania transakcji')
+
+df_without_clients_ID = df_reversed.drop(['Zewnętrzny numer ID karty ', 'Wewnętrzny numer ID karty ','Numer ID klienta', 'Nazwa'], axis=1)
+
+
+
+# Nazwy kolumn: ['Data faktury', 'Event', 'Informacje uzupełniające', 'Moc (kW)',
+#        'Energia (kWh)', 'Czas trwania transakcji',
+#        'Zewnętrzny numer ID karty ', 'Wewnętrzny numer ID karty ',
+#        'Numer ID klienta', 'Nazwa']
+
+# Informacje uzupełniające: np.: 
+#     'Channel:1, Transaction ID: 111441108, Meter values count:1: , Timestamp:2023-08-25T09:37:29Z, Values:Energy.Active.Import.Register (Outlet|Sample.Periodic|Raw): 7972915.0 Wh, ',
+#     'Channel:1, Transaction ID: 111441108, Meter values count:1: , Timestamp:2023-08-25T09:40:29Z, Values:Energy.Active.Import.Register (Outlet|Sample.Periodic|Raw): 7975398.0 Wh, ',
+
 
 # Odwrócic kolejnosc danych
 # Wykres mocy na podstawie kolumny Moc (kw)
 # przykladowa tabela zawiera tylko dane dla jednej ładowarki (typ Delta); sa jeszcze ladowarki dwoch typow (maja inaczej skonstruowane raporty, to ladowarki ABB Terra i ABB EVlunic) wszystkich ładowarek jest w sumie 50; musialabym zrobic trzy wersje 
 # dodac do analizy licznik calego zakladu
+# ? czytanie wartosci mocy co 15 minut? Dopytać
 
 
 # =============================================================================
